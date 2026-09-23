9a67d4cd9201534a7aa89795e6822850f42be87d
massimoParlanti <141170439+massimoparlanti2@users.noreply.github.com>
2026-09-23 19:15:40 +0200

    Time: 0 ms (100%), Space: 20.4 MB (42.5%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,11 @@
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        res=[]
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                if(nums[i]+nums[j]==target and i!=j):
-                    res.append(i)
-                    res.append(j)
-
-        return res[0:2]
+        visti = {}  # Mappa: valore -> indice
+        
+        for i, num in enumerate(nums):
+            complemento = target - num
+            if complemento in visti:
+                return [visti[complemento], i]
+            visti[num] = i
+            
+        return []
